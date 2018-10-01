07a0429c4e0af776badd006a60c9324eb685fbc4
Alissa Balge <alissa.balge@gmail.com>
2018-10-01 17:44:42 +0000

    added step 3

--- step3.js
+++ step3.js
@@ -5,7 +5,21 @@ var https = require("https");
 
 function getAndPrintHTML (options) {
 
-  /* Add your code here */
+  https.get(requestOptions, function (response){
+    var string = " ";
+
+  response.on("data", function (buffer) {
+    var part = buffer.toString();
+    string += part;
+
+  });
+
+
+  response.on('end', function() {
+      console.log('end', string);
+  });
+
+});
 
 }
 
@@ -13,3 +27,5 @@ var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step3.html'
 };
+
+getAndPrintHTML(requestOptions);
